cf0ba8516019066bcb7c8b05e5423b7da8488635
Mandalore <jose.arbelo4@hotmail.com>
2018-06-06 20:21:55 -0400

    Second Commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fat Elvis Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quadruple Coronary Bypass™ Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pickle Rick Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Freaky Tiki Burger', FALSE);
